f7aa3bc431dc935c3eb570dbe00c7ed0b600dabc
Miretpl <miretpl@gmail.com>
2020-03-31 17:42:29 +0200

    behavior_test - implement check with two sequence empty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTest.java
@@ -4,6 +4,8 @@ import edu.iis.mto.search.SequenceSearcherMock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class SimilarityFinderBehaviorTest {
 
     private SimilarityFinder similarityFinder = null;
@@ -15,7 +17,9 @@ class SimilarityFinderBehaviorTest {
     }
 
     @Test public void checkBehaviorWhenBothSeqAreEmpty () {
-
+        int[] seq1 = new int[]{}, seq2 = new int[]{};
+        
+        assertDoesNotThrow(() -> similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
 
     @Test public void checkBehaviorWhenFirstSeqIsEmpty () {

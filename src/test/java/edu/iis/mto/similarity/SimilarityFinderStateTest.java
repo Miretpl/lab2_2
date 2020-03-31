ba5936c9ad3228336747fed259ac30402fbec3c1
Miretpl <miretpl@gmail.com>
2020-03-31 17:57:39 +0200

    add init BeforeEach in StateTest

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -1,9 +1,19 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SequenceSearcherMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderStateTest {
 
+    private SimilarityFinder similarityFinder = null;
+    private SequenceSearcherMock sequenceSearcherMock = null;
+
+    @BeforeEach void init() {
+        sequenceSearcherMock = new SequenceSearcherMock();
+        similarityFinder = new SimilarityFinder(sequenceSearcherMock);
+    }
+    
     @Test public void checkStateWhenBothSeqAreEmpty () {
 
     }

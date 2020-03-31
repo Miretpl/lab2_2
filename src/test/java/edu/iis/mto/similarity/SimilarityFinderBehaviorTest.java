1b7845cb77e2d3c2281907e0e5d148e34a6f80d0
Miretpl <miretpl@gmail.com>
2020-03-31 11:36:39 +0200

    design state and behavior tests

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTest.java
@@ -0,0 +1,46 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+class SimilarityFinderBehaviorTest {
+
+    @Test public void checkBehaviorWhenBothSeqAreEmpty () {
+
+    }
+
+    @Test public void checkBehaviorWhenFirstSeqIsEmpty () {
+
+    }
+
+    @Test public void checkBehaviorSecondSeqIsEmpty () {
+
+    }
+
+    @Test public void checkBehaviorWhenFirstSeqIsFilled () {
+
+    }
+
+    @Test public void checkBehaviorSecondSeqIsFilled () {
+
+    }
+
+    @Test public void checkBehaviorBothSeqAreFilled () {
+
+    }
+
+    @Test public void checkBehaviorBothSeqAreNull () {
+
+    }
+
+    @Test public void checkBehaviorFirstSeqIsNull () {
+
+    }
+
+    @Test public void checkBehaviorSecondSeqIsNull () {
+
+    }
+
+    @Test public void checkBehaviorWhenSequenceSeacherIsNull () {
+
+    }
+}
---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -0,0 +1,38 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+class SimilarityFinderStateTest {
+
+    @Test public void checkStateWhenBothSeqAreEmpty () {
+
+    }
+
+    @Test public void checkStateWhenFirstSeqIsEmpty () {
+
+    }
+
+    @Test public void checkStateSecondSeqIsEmpty () {
+
+    }
+
+    @Test public void checkStateWhenFirstSeqIsFilled () {
+
+    }
+
+    @Test public void checkStateSecondSeqIsFilled () {
+
+    }
+
+    @Test public void checkStateBothSeqAreFilled () {
+
+    }
+
+    @Test public void checkStateFullMatch () {
+
+    }
+
+    @Test public void checkStateNoMatch () {
+
+    }
+}
